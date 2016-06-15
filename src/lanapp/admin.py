832e75478a0698d3173a1fb68791dcd350cee7eb
medwards147 <medwards147@gmail.com>
2016-06-15 00:08:47 -0400

    update admin to remove collapse in lanapp

--- src/lanapp/admin.py
+++ src/lanapp/admin.py
@@ -5,8 +5,8 @@ from .models import Event
 class EventAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Event Info',               {'fields': ['event_name', 'pay_button_text']}),
-        ('Date information', {'fields': ['event_start_date', 'event_end_date'], 'classes': ['collapse']}),
-        ('Location and Logistics information', {'fields': ['venue','street_address', 'city', 'state', 'zip_code'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['event_start_date', 'event_end_date']}),
+        ('Location and Logistics information', {'fields': ['venue','street_address', 'city', 'state', 'zip_code']]}),
     ]
     list_display = ('event_name', 'event_start_date', 'event_coming_soon')
     list_filter = ['event_start_date']
