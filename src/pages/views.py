640712acc9a99c379778b51396d4b04c0a65e434
medwards147 <medwards147@gmail.com>
2016-06-16 00:34:10 -0400

    updates to index for images

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,11 +4,6 @@ from django.shortcuts import render
 from .models import HomePage
 from lanapp.models import Event
 
-def get_image_check(img_obj):
-    if img_obj:
-        return img_obj
-    else:
-        return "You need to add an image in the admin"
 
 def homepage(request):
     """
@@ -19,14 +14,6 @@ def homepage(request):
     except HomePage.DoesNotExist:
         homepage = HomePage.objects.create_homepage("Automatically Created Homepage")
 
-    try:
-        banner_image = homepage.banner_image
-    except ValueError:
-        banner_image = "No banner image"
-
-    first_image = get_image_check(homepage.first_image)
-    second_image = get_image_check(homepage.second_image)
-    third_image = get_image_check(homepage.third_image)
 
     if Event.objects.all().count() == 0:  
         event = Event.objects.create_event()
@@ -35,10 +22,6 @@ def homepage(request):
     
     context = {
             "homepage": homepage,
-            "banner_image": banner_image,
-            "first_image": first_image,
-            "second_image": second_image,
-            "third_image": third_image,
             "event" : event,
     }
     
